a30010171e6f840c41bd1f96d1b6c881242525b0
ssshldm <labramario932@gmail.com>
2025-11-06 17:27:59 -0300

    CARGA INICIAL

---
+++ app/src/main/java/com/example/fitnes33/data/model/ActivityStatistics.kt
@@ -0,0 +1,7 @@
+package com.example.fitnes33.data.model
+
+data class ActivityStatistics(
+    val activityType: ActivityType,
+    val totalDuration: Long // en milisegundos
+)
+
---
+++ app/src/main/java/com/example/fitnes33/ui/theme/ActivityColors.kt
@@ -0,0 +1,15 @@
+package com.example.fitnes33.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+// Colores específicos para cada actividad
+val TransportColor = Color(0xFFFFDE21) // Amarillo
+val StudyColor = Color(0xFF9E1C69) // Magenta
+val WalkingColor = Color(0xFFBCD627) // Verde claro
+val SportColor = Color(0xFFF78636) // Naranjo
+
+// Colores de la aplicación
+val AppBackgroundColor = Color(0xFFD63A59) // Rojo de fondo
+val BackgroundWhite = Color(0xFFFFFFFF) // Blanco
+val TextDarkGray = Color(0xFF333333) // Gris oscuro
+
